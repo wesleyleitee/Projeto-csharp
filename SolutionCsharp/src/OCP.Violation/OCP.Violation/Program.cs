5701f8e2e1e43b6af9523b42bfae2dae7bfae82b
Wesley Leite <leitewesleyleite@gmail.com>
2026-01-11 15:55:52 -0300

    OCP Violation

--- SolutionCsharp/src/OCP.Violation/OCP.Violation/Program.cs
+++ SolutionCsharp/src/OCP.Violation/OCP.Violation/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace OCP.Violation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var usuarioService = new UsuarioService();
+
+            var usuarioPremium = new Usuario(
+                "Wesley",
+                "wesley@email.com",
+                TipoUsuarioEnum.Premium
+            );
+
+            var usuarioBasico = new Usuario(
+                "Ana",
+                "ana@email.com",
+                TipoUsuarioEnum.Basico
+            );
+
+            var usuarioConvidado = new Usuario(
+                "João",
+                "joao@email.com",
+                TipoUsuarioEnum.Convidado
+            );
+
+            usuarioService.CriarUsuario(usuarioPremium);
+            usuarioService.CriarUsuario(usuarioBasico);
+            usuarioService.CriarUsuario(usuarioConvidado);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SolutionCsharp/src/OCP.Violation/OCP.Violation/UsuarioService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP.Violation
+{
+    internal class UsuarioService
+    {
+        public void CriarUsuario(Usuario usuario)
+        {
+            if (usuario.Tipo == TipoUsuarioEnum.Premium)
+            {
+                EnviarEmail(usuario);
+                EnviarWhatsapp(usuario);
+            }
+            else if (usuario.Tipo == TipoUsuarioEnum.Basico)
+            {
+                EnviarEmail(usuario);
+            }
+            else if (usuario.Tipo == TipoUsuarioEnum.Convidado)
+            {
+                EnviarSms(usuario);
+            }
+        }
+
+        private void EnviarEmail(Usuario usuario)
+        {
+            Console.WriteLine($"📧 Email enviado para {usuario.Email}");
+        }
+
+        private void EnviarSms(Usuario usuario)
+        {
+            Console.WriteLine($"📱 SMS enviado para {usuario.Nome}");
+        }
+
+        private void EnviarWhatsapp(Usuario usuario)
+        {
+            Console.WriteLine($"💬 WhatsApp enviado para {usuario.Nome}");
+        }
+    }
+}
