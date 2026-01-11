dad7670487eea07cca7f4713024b2306a77a2bb6
Wesley Leite <leitewesleyleite@gmail.com>
2026-01-11 19:53:20 -0300

    comment

--- SolutionCsharp/src/OCP.Solution/OCP.Solution/Estrategias/NotificacaoUsuarioBasico.cs
+++ SolutionCsharp/src/OCP.Solution/OCP.Solution/Estrategias/NotificacaoUsuarioBasico.cs
@@ -6,6 +6,7 @@ namespace OCP.Solution.Estrategias
     {
         public TipoUsuarioEnum Tipo => TipoUsuarioEnum.Basico;
 
+        // Me devolva uma lista de coisas que sabem enviar notificação
         public IEnumerable<INotificacaoService> ObterNotificacoes()
         {
             return new INotificacaoService[]
