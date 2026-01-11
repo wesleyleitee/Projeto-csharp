7ce4104d00ed21387082c0ad54227c3d9c8d419a
Wesley Leite <leitewesleyleite@gmail.com>
2026-01-11 20:07:24 -0300

    Strategic commentary

--- SolutionCsharp/src/OCP.Solution/OCP.Solution/Estrategias-Regra/NotificacaoUsuarioBasico.cs
+++ SolutionCsharp/src/OCP.Solution/OCP.Solution/Estrategias-Regra/NotificacaoUsuarioBasico.cs
@@ -2,11 +2,15 @@
 
 namespace OCP.Solution.Estrategias
 {
+    // Classe de configuração de regra
+    // Se o usuário for Básico, então as notificações são essa aqui
     internal class NotificacaoUsuarioBasico : INotificacaoPorTipoUsuario
     {
         public TipoUsuarioEnum Tipo => TipoUsuarioEnum.Basico;
 
         // Me devolva uma lista de coisas que sabem enviar notificação
+        // Só de eu retornar um tipo interface, eu posso retornar qualquer classe que implementa a interface.
+        // Polimorfismo por substituição
         public IEnumerable<INotificacaoService> ObterNotificacoes()
         {
             return new INotificacaoService[]
