eca6f9417c4fde9b1f45bbdf0ea5b10d7f8c6fd5
Wesley Leite <leitewesleyleite@gmail.com>
2026-01-11 13:51:49 -0300

    UsuarioService

---
+++ SolutionCsharp/src/SRP.Violation/UsuarioService.cs
@@ -0,0 +1,21 @@
+﻿namespace SRP.Violation
+{
+    internal class UsuarioService
+    {
+        public void CriarUsuario(string nome, string email)
+        {
+            // 1. Validação
+            if (string.IsNullOrWhiteSpace(nome))
+                throw new Exception("Nome inválido");
+
+            if (!email.Contains("@"))
+                throw new Exception("Email inválido");
+
+            // 2. Persistência
+            Console.WriteLine("Salvando usuário no banco de dados...");
+
+            // 3. Envio de email
+            Console.WriteLine($"Enviando email de boas-vindas para {email}");
+        }
+    }
+}
